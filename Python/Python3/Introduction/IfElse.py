fbb13b535322f5e5e7769330d0303cfffd6d0f03
Scott Lewis <Scott.McKenzie.Lewis@gmail.com>
2017-08-03 14:00:09 -0400

    Python IfElse challenge

--- Python/Python3/Introduction/IfElse.py
+++ Python/Python3/Introduction/IfElse.py
@@ -1,13 +1,11 @@
 if __name__ == '__main__':
-    n = int(raw_input())
-    if (n > 100):
-        print("Error.")
+    n = int(raw_input().strip())
         
-    if (n % 2):
+    if n % 2:
         print("Weird")
-    elif ( not (n % 2) and (n >= 2 and n <= 5)):
+    elif 2 <= n <= 5:
         print("Not Weird")
-    elif ( not (n % 2) and (n >= 6 and n <= 20)):
+    elif 6 <= n <= 20:
         print("Weird")
     else:
         print("Not Weird")
